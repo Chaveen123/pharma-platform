37e9eb9a46ff088d1e57907ec196528d2b18b3b2
Chaveen <chaveenpe@gmail.com>
2026-07-03 23:24:36 +0530

    Ignore infrastructure ops directory

--- infrastructure/modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_instance_id" {
-  description = "Public IP of the Bastion EC2 instance"
-  value       = aws_instance.bastion.public_ip
-}
-
-output "bastion_security_group_id" {
-  description = "Security group ID for the Bastion EC2 instance"
-  value       = aws_security_group.bastion.id
-}
